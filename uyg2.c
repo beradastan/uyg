b0d06f3db3c115e02c955a92be55a218ed89a4e4
Bera <114724896+beradastan@users.noreply.github.com>
2022-11-10 16:24:32 +0300

    Update uyg2.c

--- uyg2.c
+++ uyg2.c
@@ -12,3 +12,33 @@ int main () {
 return 0 ;    
 
 }
+
+
+#include <stdio.h>
+int gun , km , ucret;
+char tur ; 
+int main (){
+    printf("araba turu giriniz \n");
+    scanf("%c" , &tur);
+    printf("arabayi kac gun kullandiniz \n ");
+    scanf("%d" , &gun);
+    printf("araba ile kac km yol yaptiniz \n ");
+    scanf("%d" , &km);
+
+    switch (tur)
+    {
+    case 'a':
+    case 'A': ucret=gun*20 + km*18 ; break;
+    case 'b':
+    case 'B': ucret=gun*32 + km*22 ; break;
+    case 's':
+    case 'S': ucret=gun*43 + km*28 ; break;
+    case 'p':
+    case 'P': ucret=gun*51+ km*36 ; break;
+    default:
+        break;
+    }
+    printf("odemeniz gereken tutar = %d " , ucret);
+
+    return 0 ;
+}
